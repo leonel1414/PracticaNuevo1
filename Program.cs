49359879be94e94b4ad07ba2d719840a6d9c1003
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-08-23 21:08:13 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Program
+{ 
+    class Program{ 
+        public static void Main(string[] args) {
+
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hola soy yo de nuevo jaja!!");
+            int a = 3;
+
+            if (a > 0)
+            {
+                Console.WriteLine("El valor es positivo");
+                a = 1;
+                Console.WriteLine($"el valor de a es: {a}");
+            }
+            else {
+                Console.WriteLine("El valor es negativo");
+                a = -1;
+                Console.WriteLine($"el valor de a es: {a}");
+            }
+
+        }
+    }
+}
