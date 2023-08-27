80250ef0780f121c0f3159c0b9b6a7c584d9eb1a
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-08-27 16:38:58 -0300

    Cambios

--- Program.cs
+++ Program.cs
@@ -48,6 +48,52 @@ namespace Program
                 }
             }
 
+
+
+
+            //Declaracion de Variables
+            bool varBoolean; //Alias Boolean
+            int varInt; //Alias int32
+            double varDouble;
+            string varString;//Alias String
+            DateTime varDateTime; // Alias DateTime
+
+            //Asigna un nuevo valor a cada variablea.
+
+            varBoolean = true;
+            varInt = 1123123322;
+            varDouble = 16.70;
+            varString = "asdas 92213 5+ {}";
+            varDateTime = DateTime.Now;
+
+            Console.WriteLine("Variables y tipos de datos");
+            Console.WriteLine();
+
+            //Muestra el valor de la variable
+            Console.WriteLine("Boolean = {0}", varBoolean);
+            Console.WriteLine();
+
+            Console.WriteLine("Int = {0}", varInt);
+            Console.WriteLine("Double = {0}", varDouble);
+            Console.WriteLine();
+
+            Console.WriteLine("String = {0}", varString);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Pulse una tecla para continuar ...");
+            Console.ReadLine();
+            Console.Clear();//Borra la consola
+
+            Console.WriteLine("DateTime");
+            Console.WriteLine();
+
+            Console.WriteLine("DateTime ={0} ", varDateTime);
+            Console.WriteLine();
+
+            //Muestra algunas de las propiedades disponibles en el tipo DateTime
+            Console.WriteLine("DateTime.Date = {0}", varDateTime.Date);
+
         }
 
     }
